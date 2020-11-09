974f8c303e6fc83338b619266ae2f61a60905ad5
Estrela <estrela.steinkirch@gmail.com>
2020-11-09 19:28:33 +0100

    Ajust baseurl from axios

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: "https://estrela-grades-api.herokuapp.com:8081/",
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
